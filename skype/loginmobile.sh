73c5fe2cbb05e25f9f61978c7cf7d52286243b1c
Thibault FRANCOIS <thifranc@e3r5p22.42.fr>
2016-07-16 16:47:38 +0200

    script repared, but longer

--- skype/loginmobile.sh
+++ skype/loginmobile.sh
@@ -1,9 +1,11 @@
 while read LOGIN
 do
 	ldapsearch uid=$LOGIN |
-	if grep "dn" | grep "2016" &
-		grep "mobile" | sed "s/[^0-9]*//g" | grep -E "^0[0-9]{9}$" | sed "s/^0/+33/g" >> $2
+	if grep "dn" | grep "2016"
 		then 
+			ldapsearch uid=$LOGIN |
+			grep "mobile" | sed "s/[^0-9]*//g" |
+			grep -E "^0[0-9]{9}$" | sed "s/^0/+33/g" >> $2
 			(echo SUCCESS ; exit 1)
 		else 
 			(echo FAIL ; exit 0) fi
